7e980cc688d3738904725542a7614a0910ccd50d
kaua <kauavbrandaorocha@gmail.com>
2025-11-05 18:47:34 -0300

    feat: Define o banco onde as tabelas irão ser criadas

--- db.sql
+++ db.sql
@@ -1,12 +1,14 @@
+USE projeto1
+
 CREATE TABLE clientes (
     id int AUTO_INCREMENT PRIMARY KEY,
     cliente VARCHAR(150) NOT NULL,
     cidade VARCHAR(100),
     estado VARCHAR(50)
-)
+);
 
 CREATE TABLE funcionarios (
     id int AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100),
     cargo VARCHAR(30)
-)
+);
